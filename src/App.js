92f4ac137174e9785b4fc8648b2ca781e5a6dc2b
sadhu <sad.hu@outlook.com>
2021-03-24 14:34:14 +0800

    diffrent context for dispatch & state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import {useReducer, useMemo} from 'react'
-
-import {MemoWrapper} from './components/memo-wrapper'
+import {useReducer} from 'react'
 
 import {reducer} from './reducers/reducer'
 
+import {context, share} from './contexts/context'
 
+import {Cmp1} from './components/cmp-1'
+import {Cmp2} from './components/cmp-2'
 
 
 function App() {
@@ -12,10 +13,42 @@ function App() {
   const [val, dispatch] = useReducer(reducer, 5)
 
   console.log('App render')
-  return useMemo(
-    () => <MemoWrapper val={val} dispatch={dispatch} />,
-    [val]
-  );
+  return (
+    <>
+      <pre>{val}</pre>
+      <share.Provider value={val}>
+        <context.Provider value={dispatch}>
+          <Cmp1 />
+          <Cmp2 />
+          <div className="App">
+            <header className="App-header">
+              <p>
+                Edit <code>src/App.js</code> and save to reload.
+              </p>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
+              </a>
+            </header>
+          </div>
+        </context.Provider>
+      </share.Provider>
+      <ul>
+        <li>
+          适合父代和子代组件的通信，尤其适合子代对父代组件的状态变更，通过传递 dispatch 或
+          通过 context 传递 dispatch
+        </li>
+        <li>
+          如果状态需要在父代和子代组件中共享，通过 props 或另外的 context（dispatch 的 
+          context 和状态的 context 分开）来共享状态
+        </li>
+      </ul>
+    </>
+  )
 }
 
 export default App;
--- src/components/cmp-21.js
+++ src/components/cmp-21.js
@@ -1,9 +1,14 @@
+import {useContext} from 'react'
+import {share} from '../contexts/context'
+
 export function Cmp21() {
+  const val = useContext(share)
 
   console.log('cmp 21 render')
   return (
     <div>
       <h3>cmp 21</h3>
+      <pre>{val}</pre>
     </div>
   )
 }
